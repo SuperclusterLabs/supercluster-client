aa33337f08ff8b765a0567758c4f29ac2919aa2d
Govind Mohan <info@govindmohan.com>
2022-09-29 01:58:14 -0400

    feat: make websocket connection when UI loads

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,30 +1,48 @@
+import React from 'react';
+
 import { Todos } from "./components/Todos";
 import { About } from "./components/About";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="container">
-              <Todos />
-            </div>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <div className="container">
-              <About />
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+const client = new W3CWebSocket('ws://127.0.0.1:4000/api/ws');
+
+class App extends React.Component {
+  componentWillMount() {
+    console.log("starting websocket client")
+    client.onopen = () => {
+      client.send("Hello server!");
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="container">
+                <Todos />
+              </div>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <div className="container">
+                <About />
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
