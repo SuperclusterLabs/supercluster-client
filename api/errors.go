ea7323e657394db357c7b68d705450c16538acce
Govind Mohan <info@govindmohan.com>
2022-10-16 16:13:59 -0400

    refactor: factor out errors

---
+++ api/errors.go
@@ -0,0 +1,11 @@
+package api
+
+import (
+	"errors"
+)
+
+var ErrNotFound = errors.New("File does not exist")
+var ErrFileExists = errors.New("File already exists")
+var ErrRequestUnmarshalled = errors.New("Request could not be unmarshalled")
+var ErrCannotCreate = errors.New("File could not be created")
+var ErrExistingFileRead = errors.New("Could not read existing file:")
--- api/store.go
+++ api/store.go
@@ -1,15 +1,8 @@
 package api
 
 import (
-	"errors"
 	"context"
 )
-
-var ErrNotFound = errors.New("File does not exist")
-var ErrFileExists = errors.New("File already exists")
-var ErrRequestUnmarshalled = errors.New("Request could not be unmarshalled")
-var ErrCannotCreate = errors.New("File could not be created")
-var ErrExistingFileRead = errors.New("Could not read existing file:")
 
 type Store interface {
 	Create(ctx context.Context, name, contents string) (*file, error)
