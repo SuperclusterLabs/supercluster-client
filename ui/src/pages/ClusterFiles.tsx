575cdb3145a230655dc2ec022173bcedb7f566dc
Kai Huang <kaihuang724@gmail.com>
2022-11-20 20:03:46 -0800

    feat: added table for files, inner pages on cluster UI

--- ui/src/pages/ClusterFiles.tsx
+++ ui/src/pages/ClusterFiles.tsx
@@ -1,10 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TextInput from "../components/TextInput";
 import Dropzone from "../components/Dropzone";
+
+const exampleFiles: Array<any> = [
+  {
+    name: "Example file 1",
+    cid: "zb2rhe5P4gXftAwvA4eXQ5HJwsER2owDyS9sKaQRRVQPn93bA",
+    type: "mime/image",
+    description: "Example description for example file 1",
+    size: "43000",
+    dateUploaded: "2011-08-12T20:17:46.384Z",
+    lastModified: "2021-08-12T20:17:46.384Z",
+    uploadedBy: "kaihuang.eth"
+  },
+  {
+    name: "Example file 2",
+    cid: "zb2rhe5P4gXftAwvA4eXQ5HJwsER2owDyS9sKaQRRVQPn93bA",
+    type: "mime/image",
+    description: "Example description for example file 2",
+    size: "43000",
+    dateUploaded: "2011-08-12T20:17:46.384Z",
+    lastModified: "2021-08-12T20:17:46.384Z",
+    uploadedBy: "kaihuang.eth"
+  },
+  {
+    name: "Example file 3",
+    cid: "zb2rhe5P4gXftAwvA4eXQ5HJwsER2owDyS9sKaQRRVQPn93bA",
+    type: "mime/image",
+    description: "Example description for example file 3",
+    size: "43000",
+    dateUploaded: "2011-08-12T20:17:46.384Z",
+    lastModified: "2021-08-12T20:17:46.384Z",
+    uploadedBy: "kaihuang.eth"
+  },
+]
+
 
 function ClusterFiles() {
   // TODO: Need to get the files from the Cluster
   const [search, setSearch] = useState<string>("");
+  const [files, setFiles] = useState<Array<any>>([]);
+
+  // TODO: Change to get Files from API
+  useEffect(() => {
+    setFiles(exampleFiles)
+  }, [])
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     setSearch(e.target.value);
@@ -47,6 +87,33 @@ function ClusterFiles() {
       <div className="flex mt-4">
         <Dropzone multiple={true} onChange={handleFileUpload} />
       </div>
+      <table className="table-auto border-separate border-spacing-8 text-l-slateblue-700">
+        <thead>
+          <tr>
+            <th>File type</th>
+            <th>Filename</th>
+            <th>Size</th>
+            <th>Date uploaded</th>
+            <th>Last modified</th>
+            <th>Uploaded by</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {files.map((file) => {
+            return (
+              <tr>
+                <td>{file.type}</td>
+                <td>{file.name}</td>
+                <td>{file.size}</td>
+                <td>{file.dateUploaded}</td>
+                <td>{file.lastModified}</td>
+                <td>{file.uploadedBy}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
--- ui/src/pages/ClusterMembers.tsx
+++ ui/src/pages/ClusterMembers.tsx
@@ -1,7 +1,64 @@
+import { useEffect, useState } from "react"
+import ButtonPrimary from "../components/ButtonPrimary"
+import TextInput from "../components/TextInput";
+
+const exampleMembers = [
+  {
+    id: "id_1",
+    clusters: ["exampleCluster", "exampleCluster"],
+    ethAddr: "0x6eD68a1982ac2266ceB9C1907B629649aAd9AC20",
+    ipfsAddr: "ipfs_addr"
+  },
+  {
+    id: "id_1",
+    clusters: ["exampleCluster", "exampleCluster"],
+    ethAddr: "0x6eD68a1982ac2266ceB9C1907B629649aAd9AC20",
+    ipfsAddr: "ipfs_addr"
+  },
+  {
+    id: "id_1",
+    clusters: ["exampleCluster", "exampleCluster"],
+    ethAddr: "0x6eD68a1982ac2266ceB9C1907B629649aAd9AC20",
+    ipfsAddr: "ipfs_addr"
+  },
+]
+
 function ClusterMembers() {
+  // TODO: Get members from cluster API endpoint
+  const [members, setMembers] = useState<Array<any>>([]);
+  const [address, setAddress] = useState<string>("");
+
+  // TODO: Set members from cluster API endpoint
+  useEffect(() => {
+    setMembers(exampleMembers)
+  }, [])
+
+  function handleInvite() {
+    console.log("Inviting member:", address)
+  }
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setAddress(e.target.value);
+  }
+
   return (
-    <div>
-      <h1>Cluster Members</h1>
+    <div className="flex flex-col mt-6 text-l-slateblue-700">
+      <h2 className="font-bold text-xl">Current members</h2>
+      <div className="flex flex-col space-y-3 mt-2 mb-9">
+        {members.map((member: any) => {
+          return (
+            <p key={member.id}>{member.ethAddr}</p>
+          )
+        })}
+      </div>
+      <div>
+        <h2 className="font-bold text-xl mb-4">Invite member</h2>
+        <TextInput
+          onChange={handleInputChange}
+          placeholder="Enter address or ENS"
+        />
+        <ButtonPrimary onClick={handleInvite} text="Invite member" />
+      </div>
     </div>
   )
 }
