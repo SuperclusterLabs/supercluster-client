609daf001dcb2686056ba19aee47099c8084b815
Govind Mohan <info@govindmohan.com>
2023-01-20 18:54:28 -0500

    refactor: add util package

---
+++ util/types.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"github.com/SuperclusterLabs/supercluster-client/model"
+)
+
+type AllFiles map[string]model.File
+
+type ListResponse struct {
+	Files []model.File `json:"files"`
+}
+
+type CreatePayload struct {
+	Name     string `json:"name"`
+	Contents string `json:"contents"`
+}
+
+type CreateResponse struct {
+	File model.File `json:"file"`
+}
+
+type ModifyPayload struct {
+	Contents string `json:"contents"`
+}
+
+type ModifyResponse struct {
+	File model.File `json:"file"`
+}
+
+type ResponseError struct {
+	Error string `json:"error"`
+}
+
+type AddrsResponse struct {
+	ID    string   `json:"id"`
+	Addrs []string `json:"addrs"`
+}
+
+type PinRequest struct {
+	Cid string `json:"cid"`
+}
