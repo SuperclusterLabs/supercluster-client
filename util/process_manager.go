5638ee996d4d54959fe8a48ddfc27b52abd8ea5b
Govind Mohan <info@govindmohan.com>
2023-01-29 20:06:23 -0500

    feat: add process manager and use it to manage IPFS

---
+++ util/process_manager.go
@@ -0,0 +1,60 @@
+package util
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+type ProcessManager struct {
+	exePath string
+	args    []string
+	cmd     *exec.Cmd
+}
+
+func NewProcessManager(exePath string, args []string) *ProcessManager {
+	return &ProcessManager{
+		exePath: exePath,
+		args:    args,
+	}
+}
+
+func (pm *ProcessManager) Start() error {
+	pm.cmd = exec.Command(pm.exePath, pm.args...)
+	err := pm.cmd.Start()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (pm *ProcessManager) Stop() error {
+	if pm.cmd == nil {
+		return fmt.Errorf("the process is not running")
+	}
+	return pm.cmd.Process.Signal(os.Interrupt)
+}
+
+// TODO: support windows
+func (pm *ProcessManager) pause() error {
+	if pm.cmd == nil || pm.cmd.Process == nil {
+		return fmt.Errorf("Process not running")
+	}
+	err := pm.cmd.Process.Signal(syscall.SIGSTOP)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (pm *ProcessManager) resume() error {
+	if pm.cmd == nil || pm.cmd.Process == nil {
+		return fmt.Errorf("Process not running")
+	}
+	err := pm.cmd.Process.Signal(syscall.SIGCONT)
+	if err != nil {
+		return err
+	}
+	return nil
+}
