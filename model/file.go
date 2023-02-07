c9df39180e7acf52342a3b0293d76a65bc31739d
Govind Mohan <info@govindmohan.com>
2023-02-06 21:07:06 -0500

    refactor: remove `createdAt` from files

--- model/file.go
+++ model/file.go
@@ -4,7 +4,7 @@ type File struct {
 	Name      string `json:"name"`
 	Cid       string `json:"cid"`
 	Creator   string `json:"creator"`
-	CreatedAt int64  `json:"createdAt"`
+	CreatedAt int64  `json:"-"`
 	Size      int64  `json:"size"`
 	PinType   string `json:"pinType"`
 	// TODO: whitelist of users that can access
