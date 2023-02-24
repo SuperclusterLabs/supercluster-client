6c80a477ba325c6713e7c30bf87d2aa08b7c9f3f
Govind Mohan <info@govindmohan.com>
2023-02-24 18:31:11 -0500

    chore: fix setup script, make it support darwin 64

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 
-bins_dir=build
+# SUPERCLUSTER_URL=http://localhost:3030
+SUPERCLUSTER_DIR="$(echo $HOME)/.supercluster"
+KUBO_DIR="$(echo $HOME)/.supercluster/kubo"
+IPFS_CLUSTER_SERVICE_DIR="$(echo $HOME)/.supercluster/ipfs-cluster"
+KUBO_TAR=""
+IPFS_CLUSTER_SERVICE_TAR=""
 
-if [ ! -d "../$bins_dir" ]; then
-  mkdir "../$bins_dir"
+if [ ! -d $SUPERCLUSTER_DIR ]; then
+  mkdir $SUPERCLUSTER_DIR
 fi
 
-cd "../$bins_dir"
-wget https://dist.ipfs.tech/kubo/v0.18.1/kubo_v0.18.1_linux-amd64.tar.gz
-tar -xvzf kubo_v0.18.1_linux-amd64.tar.gz
-rm kubo_v0.18.1_linux-amd64.tar.gz
+cd $SUPERCLUSTER_DIR
+mkdir clusters logs
+
+case $(uname) in
+  "Linux")
+      KUBO_TAR="kubo_v0.18.1_linux-amd64.tar.gz"
+      IPFS_CLUSTER_SERVICE_TAR="ipfs-cluster-service_v1.0.5_linux-amd64.tar.gz"
+      ;;
+  "Darwin")
+      # default to M1
+      KUBO_TAR="kubo_v0.18.1_darwin-arm64.tar.gz"
+      IPFS_CLUSTER_SERVICE_TAR="ipfs-cluster-service_v1.0.5_darwin-arm64.tar.gz"
+      ;;
+  *)
+      echo "Unsupported OS"
+      exit
+      ;;
+esac
+
+wget https://dist.ipfs.tech/kubo/v0.18.1/$KUBO_TAR
+if [ $? -ne 0 ]; then
+    exit
+fi
+wget http://dist-ipfs-tech.ipns.localhost:48084/ipfs-cluster-service/v1.0.5/$IPFS_CLUSTER_SERVICE_TAR
+if [ $? -ne 0 ]; then
+    exit
+fi
+
+tar -xvzf $KUBO_TAR
+tar -xvzf $IPFS_CLUSTER_SERVICE_TAR
+
+rm $KUBO_TAR $IPFS_CLUSTER_SERVICE_TAR
